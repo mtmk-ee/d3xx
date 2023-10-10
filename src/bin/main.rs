40c4579a63410bc37ded8034dd39961b4fb4604c
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2023-10-10 10:34:15 -1000

    Remove test binary

--- src/bin/main.rs
+++
@@ -1,9 +0,0 @@
-use std::{
-    ffi::{OsStr, OsString},
-    os::windows::prelude::OsStringExt,
-};
-
-fn main() {
-    let x = String::from_utf16(&[0xFFEF, 0x0041, 0x0042, 0x0043]).unwrap();
-    println!("{:?}", x);
-}
