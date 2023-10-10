10d348dca906a906931bac3529966b82a74c2113
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2023-10-10 10:47:29 -1000

    Fix lock poisoning

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,6 +1,6 @@
 pub mod util;
 
-use std::sync::Mutex;
+use std::{panic::catch_unwind, sync::Mutex};
 
 pub use libftd3xx_ffi::*;
 
@@ -15,13 +15,22 @@ static mut GLOBAL_LOCK: Mutex<()> = Mutex::new(()); // FIXME: is a reentrant mut
 /// For example, listing devices must be done with the lock held since the
 /// operation consists of a write followed by a read of the driver's device table,
 /// which may by invalidated at any point by another thread.
+
+#[allow(clippy::missing_panics_doc)]
 pub fn with_global_lock<F, R>(f: F) -> R
 where
-    F: FnOnce() -> R,
+    F: FnOnce() -> R + std::panic::UnwindSafe,
 {
     unsafe {
-        let _guard = GLOBAL_LOCK.lock().unwrap();
-        f()
+        // unwrap() is safe because we ensure below that the lock is not poisoned.
+        let lock = GLOBAL_LOCK.lock().unwrap();
+        match catch_unwind(f) {
+            Ok(result) => result,
+            Err(e) => {
+                drop(lock);
+                panic!("panicked while holding global lock: {e:?}");
+            }
+        }
     }
 }
 
