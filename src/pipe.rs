0b271eb7ab1e3779f1d85fe42eaf5d72f4cc4706
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2023-10-09 23:40:53 -1000

    Change Pipe to store enum instead of u8

--- src/pipe.rs
+++ src/pipe.rs
@@ -4,6 +4,8 @@ use std::{
     marker::PhantomData,
 };
 
+use num_enum::{IntoPrimitive, TryFromPrimitive};
+
 use crate::{ffi, overlapped::Overlapped, try_d3xx, D3xxError, Device, Result};
 
 type PhantomLifetime<'a> = PhantomData<&'a ()>;
@@ -29,7 +31,7 @@ type PhantomLifetime<'a> = PhantomData<&'a ()>;
 /// ```
 pub struct Pipe<'a> {
     handle: ffi::FT_HANDLE,
-    id: u8,
+    id: PipeId,
     _lifetime_constraint: PhantomLifetime<'a>,
 }
 
@@ -37,13 +39,14 @@ impl<'a> Pipe<'a> {
     pub(crate) fn new(device: &'a Device, id: PipeId) -> Self {
         Self {
             handle: device.handle(),
-            id: id as u8,
+            id,
             _lifetime_constraint: PhantomData,
         }
     }
 
+    /// Get the pipe ID.
     #[must_use]
-    pub fn id(&self) -> u8 {
+    pub fn id(&self) -> PipeId {
         self.id
     }
 
@@ -54,7 +57,7 @@ impl<'a> Pipe<'a> {
         const INTERFACE_INDEX: c_uchar = 1;
         let mut info = ffi::FT_PIPE_INFORMATION::default();
         try_d3xx!(unsafe {
-            ffi::FT_GetPipeInformation(self.handle, INTERFACE_INDEX, self.id, &mut info)
+            ffi::FT_GetPipeInformation(self.handle, INTERFACE_INDEX, u8::from(self.id), &mut info)
         })?;
         PipeInfo::try_from(info)
     }
@@ -91,7 +94,7 @@ impl<'a> Pipe<'a> {
 
     /// Aborts all pending transfers
     pub fn abort(&self) -> Result<()> {
-        try_d3xx!(unsafe { ffi::FT_AbortPipe(self.handle, self.id) })
+        try_d3xx!(unsafe { ffi::FT_AbortPipe(self.handle, u8::from(self.id)) })
     }
 
     /// Aborts all pending transfers on the specified pipe if the given result is an error.
@@ -110,13 +113,13 @@ impl<'a> Pipe<'a> {
     /// Get the timeout in milliseconds for the specified pipe.
     pub fn timeout(&self) -> Result<u32> {
         let mut timeout = 0;
-        try_d3xx!(unsafe { ffi::FT_GetPipeTimeout(self.handle, self.id, &mut timeout) })?;
+        try_d3xx!(unsafe { ffi::FT_GetPipeTimeout(self.handle, u8::from(self.id), &mut timeout) })?;
         Ok(timeout)
     }
 
     /// Set the timeout in milliseconds for the specified pipe.
     pub fn set_timeout(&self, timeout: u32) -> Result<()> {
-        try_d3xx!(unsafe { ffi::FT_SetPipeTimeout(self.handle, self.id, timeout) })
+        try_d3xx!(unsafe { ffi::FT_SetPipeTimeout(self.handle, u8::from(self.id), timeout) })
     }
 
     /// Asynchronous read into the given buffer.
@@ -130,7 +133,7 @@ impl<'a> Pipe<'a> {
         let mut overlapped = Overlapped::new(self.handle)?;
         self.maybe_abort(ffi::util::read_pipe_async(
             self.handle,
-            self.id,
+            u8::from(self.id),
             buf,
             overlapped.inner_mut(),
         ))?;
@@ -148,7 +151,7 @@ impl<'a> Pipe<'a> {
         let mut overlapped = Overlapped::new(self.handle)?;
         self.maybe_abort(ffi::util::write_pipe_async(
             self.handle,
-            self.id,
+            u8::from(self.id),
             buf,
             overlapped.inner_mut(),
         ))?;
@@ -158,64 +161,69 @@ impl<'a> Pipe<'a> {
 
 impl<'a> Write for Pipe<'a> {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        let res = ffi::util::write_pipe(self.handle, self.id, buf);
+        let res = ffi::util::write_pipe(self.handle, u8::from(self.id), buf);
         Ok(self.maybe_abort(res)?)
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
-        try_d3xx!(unsafe { ffi::FT_FlushPipe(self.handle, self.id) })?;
+        try_d3xx!(unsafe { ffi::FT_FlushPipe(self.handle, u8::from(self.id)) })?;
         Ok(())
     }
 }
 
 impl<'a> Read for Pipe<'a> {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let res = ffi::util::read_pipe(self.handle, self.id, buf);
+        let res = ffi::util::read_pipe(self.handle, u8::from(self.id), buf);
         Ok(self.maybe_abort(res)?)
     }
 }
 
 /// Identifies a unique read/write pipe on a device.
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, TryFromPrimitive, IntoPrimitive)]
 #[repr(u8)]
 pub enum PipeId {
+    /// Input pipe 0.
     In0 = 0x82,
+    /// Input pipe 1.
     In1 = 0x83,
+    /// Input pipe 2.
     In2 = 0x84,
+    /// Input pipe 3.
     In3 = 0x85,
+    /// Output pipe 0.
     Out0 = 0x02,
+    /// Output pipe 1.
     Out1 = 0x03,
+    /// Output pipe 2.
     Out2 = 0x04,
+    /// Output pipe 3.
     Out3 = 0x05,
-}
-
-impl TryFrom<u8> for PipeId {
-    type Error = ();
-
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        match value {
-            0x82 => Ok(Self::In0),
-            0x83 => Ok(Self::In1),
-            0x84 => Ok(Self::In2),
-            0x85 => Ok(Self::In3),
-            0x02 => Ok(Self::Out0),
-            0x03 => Ok(Self::Out1),
-            0x04 => Ok(Self::Out2),
-            0x05 => Ok(Self::Out3),
-            _ => Err(()),
-        }
-    }
 }
 
 /// The type of a pipe.
 ///
 /// This is used to determine the type of transfer to use.
+///
+/// # References
+/// - <https://www.keil.com/pack/doc/mw/USB/html/_u_s_b__endpoints.html>
 #[allow(clippy::module_name_repetitions)]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
 pub enum PipeType {
+    /// Bidirectional control transfer.
+    ///
+    /// Reserved for the host to send/request configuration information using endpoint zero.
     Control = 0,
+    /// Isochronous transfer.
+    ///
+    /// Used for time-critical data transfers where data integrity is not critical.
     Isochronous = 1,
+    /// Bulk transfer.
+    ///
+    /// Used for miscellaneous transfers where data integrity is required.
     Bulk = 2,
+    /// Interrupt transfer.
+    ///
+    /// Used in cases where polling intervals are defined.
     Interrupt = 3,
 }
 
@@ -312,12 +320,12 @@ mod tests {
         assert_eq!(PipeId::try_from(0x03), Ok(PipeId::Out1));
         assert_eq!(PipeId::try_from(0x04), Ok(PipeId::Out2));
         assert_eq!(PipeId::try_from(0x05), Ok(PipeId::Out3));
-        assert_eq!(PipeId::try_from(0x00), Err(()));
-        assert_eq!(PipeId::try_from(0x01), Err(()));
-        assert_eq!(PipeId::try_from(0x06), Err(()));
-        assert_eq!(PipeId::try_from(0x81), Err(()));
-        assert_eq!(PipeId::try_from(0x86), Err(()));
-        assert_eq!(PipeId::try_from(0xFF), Err(()));
+        assert!(PipeId::try_from(0x00).is_err());
+        assert!(PipeId::try_from(0x01).is_err());
+        assert!(PipeId::try_from(0x06).is_err());
+        assert!(PipeId::try_from(0x81).is_err());
+        assert!(PipeId::try_from(0x86).is_err());
+        assert!(PipeId::try_from(0xFF).is_err());
     }
 
     #[test]
