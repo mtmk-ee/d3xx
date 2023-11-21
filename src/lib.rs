4858f0b504d127acca29c6a185026bde5aa01ffc
Mitchell Matsumori-Kelly <65527685+mtmk-ee@users.noreply.github.com>
2023-11-21 12:13:56 -1000

    Add more info to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,57 @@
-//! This crate provides a safe Rust wrapper around FTDI's D3XX driver API.
+//! This crate provides a safe, idiomatic Rust wrapper around FTDI's D3XX driver API.
 //!
-//! **Disclaimer:** this crate is unofficial, and is not affiliated with FTDI in any way.
+//! # Disclaimer
 //!
-//! **Note:** this crate is still in early development and is not yet ready for production use.
+//! This crate is unofficial and is not affiliated with FTDI in any way.
+//!
+//! The crate is still in early development and is not yet ready for production use.
 //! Feedback and contributions are welcome!
 //!
-//! The D3XX driver provides a low-level interface for interacting with `FT60x` devices.
-//! It is intended to be used in conjunction with the `FT60x` "Super Speed" series of ICs, which provide
-//! a USB 3.0 interface for transferring data at high speeds.
+//! # What This Crate Does
 //!
-//! The primary interface for interacting with devices is the [`Device`] struct. It provides methods
-//! for reading, writing, configuration, and more. See the [`Device`] documentation for more details.
+//! This crate provides much of the functionality of the D3XX API, including:
+//! - Device enumeration
+//! - Reading device configurations
+//! - Pipe I/O
+//! - GPIO control
+//! - Notifications
+//! - Overlapped (Asynchronous) I/O
+//!
+//! This crate does not wrap functionality for configuring the device. If it is deemed necessary,
+//! the unsafe FFI functions may be called directly. However, it is recommended to use the
+//! [FT60X Chip Configuration Programmer](https://ftdichip.com/utilities/) instead for this purpose.
+//!
+//! # Background
+//!
+//! Future Technology Devices International (FTDI) produces the FT60X series of chips (e.g. FT601),
+//! which act as SuperSpeed USB 3.0 to FIFO bridges. FTDI provides a proprietary driver for these chips,
+//! called D3XX, which exposes a low-level API for interacting with the devices through its DLL/shared library.
+//!
+//! ## Brief Refresher on USB 3.0
+//!
+//! ### Terminology
+//! USB peripherals contain a series of numbered endpoints, which are essentially physical data buffers. Each endpoint may contain
+//! one or two buffers, corresponding to the direction of data flow (IN or OUT). D3XX devices have 8 endpoints, 4 each for IN and OUT transfers.
+//! The software representation of an endpoint is known as a "pipe" and is unidirectional.
+//!
+//! Endpoints are collected into "interfaces", which are logical groupings of endpoints that serve a common purpose.
+//! These interfaces are then collected into "configurations", which are collections of interfaces that represent
+//! a complete set of functionality for the device. A device may have multiple configurations, but only one may be active
+//! at a time.
+//!
+//! ### Transfers
+//!
+//! Data is transferred to and from the device in packets called "transactions". There are four types of transfers
+//! that can be performed:
+//!
+//! - Control transfers: mainly used for configuration and identification by the host.
+//! - Bulk transfers: used for large data transfers where correctness is prioritized over throughput.
+//! - Isochronous transfers: used for streaming data where throughput is prioritized over correctness.
+//! - Interrupt transfers: used for small data transfers that require low latency.
+//!
+//! For example, a keyboard or mouse would use interrupt transfers, while a web camera would use isochronous transfers.
+//!
+//!
 //!
 //! # Simple Example
 //!
@@ -41,11 +82,17 @@
 //! # Error Handling
 //!
 //! The documentation on most functions returning a `Result<T, D3xxError>` does not include an
-//! explanation about the error conditions. This is because the D3XX documentation is vague,
-//! providing little information about what errors can occur and under what circumstances.
+//! explanation about the error conditions. This is because in most cases the D3XX documentation
+//! does not provide any information about what errors can occur and under what circumstances.
 //!
-//! Catching specific errors in an API-backed manner is not possible, so it is recommended to
-//! use a catch-all approach in most cases.
+//! Because of the lack of a clear specification it is not wise to attempt to handle specific
+//! errors in a programmatic manner, as the error conditions may change in future versions of the
+//! D3XX API. Instead, it is recommended to use a catch-all approach in most cases.
+//!
+//! # Further Reading
+//!
+//! It is recommended to read the [D3XX Programmers Guide](https://ftdichip.com/wp-content/uploads/2020/07/AN_379-D3xx-Programmers-Guide-1.pdf)
+//! for more information about the capabilities provided by the D3XX API.
 
 #![warn(clippy::all, clippy::pedantic, clippy::cargo, missing_docs)]
 // Allow missing error documentation since the D3XX documentation is vague about error conditions.
